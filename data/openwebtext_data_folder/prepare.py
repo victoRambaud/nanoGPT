5f2fe7644708ea2a80a9c1cf331278a9ded56065
Victor Rambaud <victor.rambaud@gmail.com>
2025-12-15 10:11:04 +0100

    prepare several mul

--- data/openwebtext_data_folder/prepare.py
+++ data/openwebtext_data_folder/prepare.py
@@ -64,25 +64,29 @@ if __name__ == '__main__':
         num_proc=num_proc,
     )
     total_batches = 4096
+    multipliers = [2, 4, 8, 16]
     # concatenate all the ids in each dataset into one large file we can use for training
     main_path = "/lustre/fswork/projects/rech/fku/uir17ua/data"
     for split, dset in tokenized.items():
-        arr_len = np.sum(dset['len'], dtype=np.uint64)
+        if split == "val":
+            for mul in multipliers:
+                total_batches = 1024 * mul
+                arr_len = np.sum(dset['len'], dtype=np.uint64)
 
-        filename = os.path.join(os.path.dirname(__file__), f'{split}_{total_batches}.bin')
-        dtype = np.uint16 # (can do since enc.max_token_value == 50256 is < 2**16)
-        arr = np.memmap(filename, dtype=dtype, mode='w+', shape=(arr_len,))
-        
+                filename = os.path.join(os.path.dirname(__file__), f'{split}_{total_batches}.bin')
+                dtype = np.uint16 # (can do since enc.max_token_value == 50256 is < 2**16)
+                arr = np.memmap(filename, dtype=dtype, mode='w+', shape=(arr_len,))
+                
 
-        idx = 0
-        for batch_idx in tqdm(range(total_batches), desc=f'writing {filename}'):
-            # Batch together samples for faster write
-            batch = dset.shard(num_shards=total_batches, index=batch_idx, contiguous=True).with_format('numpy')
-            arr_batch = np.concatenate(batch['ids'])
-            # Write into mmap
-            arr[idx : idx + len(arr_batch)] = arr_batch
-            idx += len(arr_batch)
-        arr.flush()
+                idx = 0
+                for batch_idx in tqdm(range(total_batches), desc=f'writing {filename}'):
+                    # Batch together samples for faster write
+                    batch = dset.shard(num_shards=total_batches, index=batch_idx, contiguous=True).with_format('numpy')
+                    arr_batch = np.concatenate(batch['ids'])
+                    # Write into mmap
+                    arr[idx : idx + len(arr_batch)] = arr_batch
+                    idx += len(arr_batch)
+                arr.flush()
 
     # train.bin is ~17GB, val.bin ~8.5MB
     # train has ~9B tokens (9,035,582,198)
