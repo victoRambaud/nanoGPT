7762596e01f37e04faa7b6cb363a278248a5b179
Victor Rambaud <victor.rambaud@gmail.com>
2025-12-17 16:15:31 +0100

    wm full

--- run_job_wm.sh
+++ run_job_wm.sh
@@ -21,7 +21,7 @@
 #SBATCH --hint=nomultithread
 
 #SBATCH --qos=qos_gpu_h100-t4
-#SBATCH --time=100:00:00
+#SBATCH --time=72:00:00
 #SBATCH --output=logs/gpu_job%j.out
 #SBATCH --error=errors/gpu_job%j.out
 
--- train.py
+++ train.py
@@ -40,14 +40,14 @@ if __name__ == "__main__":
     # default config values designed to train a gpt2 (124M) on OpenWebText
     # I/O
     out_dir = "out"
-    wandb_run_name = "WM_L12_n768_base1024_rank32_ls0.0id_61"
-    ckpt_name = "checkpoint-72000"
+    wandb_run_name = ""
+    ckpt_name = ""
     eval_interval = 2000
     log_interval = 1
     eval_iters = 200
     eval_only = False  # if True, script exits right after the first eval
     always_save_checkpoint = True  # if True, always save a checkpoint after each eval
-    init_from = "resume"  # 'scratch' or 'resume' or 'gpt2*'
+    init_from = "scratch"  # 'scratch' or 'resume' or 'gpt2*'
 
     # wandb logging
     wandb_log = True  # disabled by default
@@ -86,7 +86,7 @@ if __name__ == "__main__":
             wandb_run_name = f"ROPE_L{n_layer}_n{n_embd}"
 
     # adamw optimizer
-    learning_rate = 5e-4 # max learning rate
+    learning_rate = 6e-4 # max learning rate
     max_iters = 600000 # total number of training iterations
     weight_decay = 1e-1
     beta1 = 0.9
@@ -94,9 +94,9 @@ if __name__ == "__main__":
     grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
     # learning rate decay settings
     decay_lr = True # whether to decay the learning rate
-    warmup_iters = 4000 # how many steps to warm up for
+    warmup_iters = 2000 # how many steps to warm up for
     lr_decay_iters = 600000 # should be ~= max_iters per Chinchilla
-    min_lr = 0.00001 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
+    min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 
     # DDP settings
     backend = "nccl"  # 'nccl', 'gloo', etc.
