6ce07c3bf6f93da45b48cc8704e41f4334b40e84
Victor Rambaud <victor.rambaud@gmail.com>
2026-01-05 13:34:13 +0100

    eval wm and rope

---
+++ config/eval_gpt2_wm.py
@@ -0,0 +1,81 @@
+# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
+# launch as the following (e.g. in a screen session) and wait ~5 days:
+# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+
+out_dir = "out"
+wandb_run_name = "WM_L12_n768_base1024_rank8_ls0.0id_919"
+ckpt_path = "/lustre/fswork/projects/rech/fku/uir17ua/dev/nanoGPT/out/WM_L12_n768_base1024_rank8_ls0.0id_919/checkpoint-84000/ckpt.pt"
+eval_interval = 2000
+log_interval = 1
+eval_iters = 200
+eval_only = True  # if True, script exits right after the first eval
+always_save_checkpoint = True  # if True, always save a checkpoint after each eval
+init_from = "resume"  # 'scratch' or 'resume' or 'gpt2*'
+
+# wandb logging
+wandb_log = True  # disabled by default
+wandb_project = "gpt2-openweb-eval"
+wandb_mode = "offline"
+
+# data
+dataset = "openwebtext_data_folder"
+gradient_accumulation_steps = 5 * 8  # used to simulate larger batch sizes
+batch_size = 12  # if gradient_accumulation_steps > 1, this is the micro-batch size
+block_size = 1024
+
+# model
+n_layer = 12
+head_dim = 64
+n_embd = 768
+dropout = 0.0  # for pretraining 0 is good, for finetuning try 0.1+
+bias = False  # do we use bias inside LayerNorm and Linear layers?
+rope: bool = False
+cope: bool = False
+working_memory: bool = True
+episodic_memory: bool = False
+n_episodic_memory: bool = False
+transformer_type: str = "WM"
+n_working_memory: bool = True
+inv_scale_attn: bool = True
+em_wm: bool = False
+n_approx_steps: int = -1
+dt_rank: int = 8
+base_freq = block_size
+block_max_init: float = 1.0
+block_layer_scaling_ratio: float = 0.
+
+# run name
+# run name
+if init_from == "scratch":
+    if transformer_type == "WM":
+        wandb_run_name = f"WM_L{n_layer}_n{n_embd}_base{base_freq}_rank{dt_rank}_ls{block_layer_scaling_ratio}"
+    elif transformer_type == "EM":
+        wandb_run_name = f"EM_L{n_layer}_n{n_embd}_base{base_freq}_rank{dt_rank}_ls{block_layer_scaling_ratio}"
+    elif transformer_type == "nEMWM":
+        wandb_run_name = f"nEMWM_invscale_{inv_scale_attn}_L{n_layer}_n{n_embd}_base{base_freq}_rank{dt_rank}_ls{block_layer_scaling_ratio}"
+    elif transformer_type == "nWM" and not rope:
+        wandb_run_name = f"nWM_invscale_{inv_scale_attn}_L{n_layer}_n{n_embd}_base{base_freq}_rank{dt_rank}_ls{block_layer_scaling_ratio}"
+    elif transformer_type == "nWM" and rope:
+        wandb_run_name = f"nROPE_invscale_{inv_scale_attn}_L{n_layer}_n{n_embd}_base{base_freq}_rank{dt_rank}_ls{block_layer_scaling_ratio}"
+    elif transformer_type == "nEM":
+        wandb_run_name = f"nEM_invscale_{inv_scale_attn}_L{n_layer}_n{n_embd}_base{base_freq}_rank{dt_rank}_ls{block_layer_scaling_ratio}"
+    elif cope:
+        wandb_run_name = f"COPE_L{n_layer}_n{n_embd}"
+        transformer_type = "WM"
+    else:
+        rope = True
+        wandb_run_name = f"ROPE_L{n_layer}_n{n_embd}"
+        transformer_type = "WM"
+
+# adamw optimizer
+learning_rate = 3e-4 # max learning rate
+max_iters = 600000 # total number of training iterations
+weight_decay = 1e-1
+beta1 = 0.9
+beta2 = 0.95
+grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
+# learning rate decay settings
+decay_lr = True # whether to decay the learning rate
+warmup_iters = 4000 # how many steps to warm up for
+lr_decay_iters = 600000 # should be ~= max_iters per Chinchilla
+min_lr = 1e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
---
+++ run_eval_rope.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --output=TravailGPU%j.out # fichier de sortie (%j = job ID)
+#SBATCH --error=TravailGPU%j.err # fichier d’erreur (%j = job ID)
+#SBATCH --job-name=EV-ROPE
+#SBATCH --account=fku@h100
+##SBATCH -A fku@cpu
+
+##SBATCH --partition=gpu_p2
+##SBATCH --partition=gpu_p4
+#SBATCH -C h100
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4
+
+#SBATCH --cpus-per-task=10
+##SBATCH --cpus-per-task=3
+##SBATCH --cpus-per-task=6
+##SBATCH --cpus-per-task=8
+
+#SBATCH --hint=nomultithread
+
+##SBATCH --qos=qos_gpu_h100-t4
+#SBATCH --qos=qos_gpu_h100-dev
+#SBATCH --time=1:00:00
+#SBATCH --output=logs/gpu_job%j.out
+#SBATCH --error=errors/gpu_job%j.out
+
+
+#######################################################################################
+
+
+module load arch/h100
+module load pytorch-gpu/py3/2.6.0
+export PYTHONPATH=/lustre/fswork/projects/rech/fku/uir17ua/python_libs:$PYTHONPATH
+
+
+# source activate mfa
+# set -x
+
+# srun --ntasks=4 python -u train.py config/train_gpt2.py
+torchrun --standalone --nproc_per_node=4 train.py config/eval_gpt2_rope.py
+# sbatch job.sh
