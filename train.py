d4cb06927815fd94e2b675637488ad78c4ef2d32
Victor Rambaud <victor.rambaud@gmail.com>
2026-07-31 16:09:30 +0200

    sfsfs

--- train.py
+++ train.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     eval_only = False  # if True, script exits right after the first eval
     always_save_checkpoint = True  # if True, always save a checkpoint after each eval
     init_from = "scratch"  # 'scratch' or 'resume' or 'gpt2*'
-    eval_blimp = True
+    eval_blimp = False
 
     # wandb logging
     wandb_log = True  # disabled by default
@@ -340,6 +340,7 @@ if __name__ == "__main__":
     model.to(device)
 
     eval_mode = "blimp"
+    eval_blimp = False
     if eval_only:
         eval_blimp = True
 
